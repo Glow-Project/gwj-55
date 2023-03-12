ba23b9e87eb6d60d51b860f1aaa918f3efc1b9ca
Raphael Pour <info@raphaelpour.de>
2023-03-12 12:42:40 +0100

    game: add explosion when sheep falls into abyss

--- game.gd
+++ game.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
+var explosion_scene = preload("res://explosion.tscn")
+
 func _on_basket_item_collected(body:Node2D):
-	body.hide()
+	var explosion:Explosion = explosion_scene.instantiate()
+	explosion.global_position = body.global_position
+	explosion.size = body.global_scale.x
+	add_child(explosion)
+	explosion.emitting = true
+	body.queue_free()
 	
 	$Counter.add_score(1)
