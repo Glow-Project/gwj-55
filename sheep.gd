4f881e25c6c3c9de416075aef3ab44e08f99a95d
Raphael Pour <info@raphaelpour.de>
2023-03-12 13:03:05 +0100

    sheep: emit sheep on event

--- sheep.gd
+++ sheep.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 class_name Sheep
 
-signal abyss_reached
+signal abyss_reached(body:Node2D)
 
 @export var abyss_y = 0.0
 @export_range(0.5,5) var speed = 1.0
@@ -10,13 +10,14 @@ signal abyss_reached
 var signal_emitted = false
 
 func _ready():
+	scale = Vector2(size,size)
 	$Sheep.scale = Vector2(size,size)
 	$CollisionPolygon2D.scale = Vector2(size,size)
 	gravity_scale = speed
 
 func _process(delta)->void:
 	if global_position.y <= abyss_y and  !signal_emitted:
-		emit_signal("abyss_reached")
+		emit_signal("abyss_reached", self)
 
 func random()->void:
 	speed = randf_range(0.5,5)
