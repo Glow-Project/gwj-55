3181e6fd572c3f106d623152c9ca2880e8db2b3b
Raphael Pour <info@raphaelpour.de>
2023-03-14 23:50:27 +0100

    game: add sounds

--- game.gd
+++ game.gd
@@ -7,10 +7,36 @@ var explosions = [
 	preload("res://explosion_2.mp3")
 ]
 
+var rewards = [
+	preload("res://chord1.mp3"),
+	preload("res://chord2.mp3"),
+	preload("res://chord3.mp3"),
+	preload("res://chord4.mp3"),
+	preload("res://chord5.mp3"),
+	preload("res://chord6.mp3"),
+]
+
+var punishments = [
+	preload("res://bad_chord1.mp3"),
+	preload("res://bad_chord2.mp3"),
+	preload("res://bad_chord3.mp3"),
+	preload("res://bad_chord4.mp3"),
+]
+
 func _on_basket_item_collected(body:Node2D):
 	body.queue_free()
-	$Counter.add_score(1)
 	$Tacho.inc()
+	
+	if $Tacho.score > 0 && $Tacho.score % 5 == 0:
+		var fx = AudioStreamPlayer.new()
+		fx.stream = rewards[randi_range(0,len(rewards)-1)]
+		fx.set_bus("RewardFX")
+		add_child(fx)
+		fx.play()
+	
+	if $Tacho.score == $Tacho.total:
+		get_tree().change_scene_to_file("res://game_over.tscn")
+	
 
 func _on_spawner_sheep_reached_abyss(body):
 	$Tacho.dec()
@@ -20,8 +46,19 @@ func _on_spawner_sheep_reached_abyss(body):
 	
 	var fx = AudioStreamPlayer.new()
 	fx.stream = explosions[randi_range(0,len(explosions)-1)]
+	fx.set_bus("ExplosionFX")
 	add_child(fx)
 	fx.play()
 	explosion.emitting = true
 
 	body.queue_free()
+	
+	if $Tacho.score < 0 && $Tacho.score % 5 == 0:
+		var fx2 = AudioStreamPlayer.new()
+		fx2.stream = punishments[randi_range(0,len(punishments)-1)]
+		fx2.set_bus("RewardFX")
+		add_child(fx2)
+		fx2.play()
+	
+	if $Tacho.score == -$Tacho.total:
+		get_tree().change_scene_to_file("res://game_over.tscn")
