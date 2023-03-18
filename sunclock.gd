5a2f7d9e5fffef3a26addac905ca86456ea26ebf
Raphael Pour <info@raphaelpour.de>
2023-03-18 20:21:48 +0100

    sunclock: use tweens for more smooth rotations #9

--- sunclock.gd
+++ sunclock.gd
@@ -2,16 +2,10 @@ extends Node2D
 
 signal sunrise()
 
-@onready var step:int = 7
-@onready var degrees:int = 0
+@export var duration:int = 30
 
-func _on_timer_timeout():
-	$Clock.global_rotation_degrees+=step
-	
-	# use dedicated degree variable, since the global_rotation_degrees variable
-	# wraps around
-	degrees += step
-	if degrees >= 360:
-		$Timer.stop()
-		emit_signal("sunrise")
-	$ClockSound.volume_db = min($ClockSound.volume_db+1, -25)
+func _ready():
+	var tween = create_tween()
+	tween.tween_property($Clock, "global_rotation_degrees", 360, duration)
+	tween.tween_property($ClockSound, "volume_db", -25, duration)
+	tween.tween_callback(func():emit_signal("sunrise"))
