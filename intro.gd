730b5e2434ed5a1d6cc79f7c6b18b6e67981745c
Raphael Pour <info@raphaelpour.de>
2023-03-18 16:45:19 +0100

    intro: finalize, make main scene

--- intro.gd
+++ intro.gd
@@ -5,7 +5,5 @@ extends Node2D
 func _ready():
 	$Animation.play("intro")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_animation_animation_finished(anim_name):
+	get_tree().change_scene_to_file("res://menu.tscn")
