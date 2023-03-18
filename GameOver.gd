7024f2c9a4e3f087d136b8198a496533c801c653
Raphael Pour <info@raphaelpour.de>
2023-03-18 08:14:40 +0100

    game over: cleanup

--- GameOver.gd
+++ GameOver.gd
@@ -1,24 +1,12 @@
 extends Node2D
 class_name GameOver
 
-var descriptions = {
-	"asleep": {
-		"won": "<whispering>Sleep well, my dear! I'm proud of all of your %d sheeps. Sweet dreams!</whispering>",
-		"lost": "Have you ever had a dream, Neo, that you were so sure was real? What if you were unable to wake from that dream? How would you know the difference between the dream world and the real world, with only %d sheeps?"
-	},
-	"fully_awake": {
-		"won": "YOU. ARE. ALIVE! Sleep is for the weak! You avoided to count %d sheeps.",
-		"lost": "FATALITY! You're sleepwalking like a zombie...mentaly wasted. You only got %d sheeps."
-	},
-}
-
 var win_sound = preload("res://win_musicbox.mp3")
 var loose_sound = preload("res://loose_sound.mp3")
 var alternate_win_sound = preload("res://alarm_clock_eriklindmanmata.mp3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print_debug(Globals.animation)
 	$Emoji.play(Globals.animation)
 	$Description.text = Globals.description % abs(Globals.score)
 
@@ -28,5 +16,4 @@ func _ready():
 		$Sound.stream = loose_sound
 	else:
 		$Sound.stream = alternate_win_sound
-
 	$Sound.play()
