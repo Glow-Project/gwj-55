1474de88d1789beed3b08e3c688d3cfb8dd2c668
Raphael Pour <info@raphaelpour.de>
2023-03-24 18:27:48 +0100

    menu: add quit button

--- menu.gd
+++ menu.gd
@@ -9,3 +9,6 @@ func _on_start_button_pressed():
 
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file("res://settings.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
